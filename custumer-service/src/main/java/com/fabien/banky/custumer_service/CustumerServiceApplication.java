f228f63984e815f84cc05e11fc44b1b57bc80e10
flautru <flautru@gmail.com>
2025-06-13 13:50:58 +0200

    First Service and Docker file

---
+++ custumer-service/src/main/java/com/fabien/banky/custumer_service/CustumerServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fabien.banky.custumer_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustumerServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CustumerServiceApplication.class, args);
+	}
+
+}
